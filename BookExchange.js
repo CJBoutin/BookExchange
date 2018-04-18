c47931ae897ebf0d53372efa7a9b8fde625f08d1
purplepharaoh <35539772+purplepharaoh@users.noreply.github.com>
2018-04-18 19:45:27 -0400

    Add files via upload

--- BookExchange.js
+++ BookExchange.js
@@ -10,15 +10,28 @@ var loggedIn = false;
 //attempting to access a page you cannot access
 //without being logged in (as well as on ready)
 $(document).ready(validateSession(window.location.pathname));
-$(document).on("");
+$(document).ready(function(){
+$("#loginForm").submit(function(e){
+   e.preventDefault();
+   login($("#loginForm"));
+});
 
+$("#registerForm").submit(function(e){
+   e.preventDefault();
+   createAccount($("#registerForm"));
+});
 
+$("#listingForm").submit(function(e){
+   e.preventDefault
+   addListing($("#listingForm"));
+});
+});
 //this ensures that a name was entered i.e. no digits hyphens and ' allowed
 //because they exist in some names
 function validateFirst(first){
     var firstCheck = new RegExp(/^[a-zA-Z]+((-?|'?)[a-zA-Z]*)*$/);
-    console.log(firstCheck.test(first.value));
-    if(!firstCheck.test(first.value)){
+    console.log(firstCheck.test(first));
+    if(!firstCheck.test(first)){
         $("#flabel").css("color","red");
         return false;
     }
@@ -29,8 +42,8 @@ function validateFirst(first){
 //because they exist in some names
 function validateLast(last){
     var lastCheck = new RegExp(/^[a-zA-Z]+((-?|'?)[a-zA-Z]*)*$/);
-    console.log(lastCheck.test(last.value));
-    if(!lastCheck.test(last.value)){
+    console.log(lastCheck.test(last));
+    if(!lastCheck.test(last)){
         $("#llabel").css("color","red");
         return false;
     }
@@ -40,12 +53,22 @@ function validateLast(last){
 //this makes sure an email address was entered i.e. blah@blah.blah
 function validateEmail(email,msgLoc){
     var emailCheck = new RegExp(/[^@\s]+@(\w|\d)+(.(\w|\d)+)+/);
-    console.log("email check:"+emailCheck.test(email.value));
-    if(!emailCheck.test(email.value)){
+    console.log("email check:"+emailCheck.test(email));
+    if(!emailCheck.test(email)){
         $("#"+msgLoc).css("color","red");
         return false;
     }
     return true;
+}
+
+function validatePhone(phone,msgLoc){
+   var phoneCheck = new RegExp(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/);
+   console.log("phone check: " + phoneCheck.test(phone));
+   if(!phoneCheck.test(phone)){
+      $("#"+msgLoc).css("color","red");
+      return false;
+   }
+   return true;
 }
 
 //this function checks the password for correctness then checks if it matches
@@ -57,20 +80,21 @@ function compPasswd(passwd, passwdcheck){
     var passwdCheckDigit = new RegExp(/\d/);
     var passwdCheckSpecChar = new RegExp(/[.,?!@#$%^&*+=]/);
     var passwdCheckIllegalChar = new RegExp(/\s?[;:'"<>\/\\\|\[\{\}\]-_`~]/);
-    console.log(passwdCheckIllegalChar.test("has illegal chars: " +passwd.value) ||
-                !(passwdCheckUpChar.test(passwd.value) &&
-                  passwdCheckLowChar.test(passwd.value) &&
-                  passwdCheckDigit.test(passwd.value) &&
-                  passwdCheckSpecChar.test(passwd.value)));
-    if(passwdCheckIllegalChar.test(passwd.value) ||
-       !(passwdCheckUpChar.test(passwd.value) &&
-         passwdCheckLowChar.test(passwd.value) &&
-         passwdCheckDigit.test(passwd.value) &&
-         passwdCheckSpecChar.test(passwd.value))){
+    console.log("Pass verification issue: " + passwdCheckIllegalChar.test(passwd) ||
+                !(passwdCheckUpChar.test(passwd) &&
+                  passwdCheckLowChar.test(passwd) &&
+                  passwdCheckDigit.test(passwd) &&
+                  passwdCheckSpecChar.test(passwd)));
+    if(passwdCheckIllegalChar.test(passwd) ||
+       !(passwdCheckUpChar.test(passwd) &&
+         passwdCheckLowChar.test(passwd) &&
+         passwdCheckDigit.test(passwd) &&
+         passwdCheckSpecChar.test(passwd))){
            $("#plabel").css("color","red");
            verify=false;
+          console.log("Password: " +passwd+ " is valid: " + verify);
        }
-    if(passwd.value != passwdcheck.value){
+    if(passwd != passwdcheck){
         $("#pclabel").css("color","red");
         verify=false;
     }
@@ -82,20 +106,22 @@ function createAccount(form){
     //uses a variable in order that all fields be evaluated before stoping
     //allows the client to see all mistakes at first input, rather than one
     //at each submit
-    var valid=true;
     //if(!validateFirst(form.first)) valid=false;
     //else $("#flabel").css("color","black");
     //if(!validateLast(form.last)) valid=false;
     //else $("#llabel").css("color","black");
-    if(!validateEmail(form.email, "elabel")) valid=false;
+
+    var valid=true;
+
+    if(!validateEmail($("#registerEmail").val(), "elabel")) valid=false;
     else $("#elabel").css("color","black");
-    if(!validatePass(form.passwd,"plabel")) valid=false;
-    else $("#plabel").css("color","black");
-    if(!compPasswd(form.passwd,form.passwdconf)) valid=false;
-    else $("#pclabel").css("color","black");
+    if(!compPasswd($("#registerPassword").val(),$("#registerPasswordConfirm").val())) valid=false;
+    else {$("#pclabel").css("color","black"); $("#plabel").css("color","black");}
+    if(!validatePhone($("#registerPhone").val(),"phlabel")) valid=false;
+    else $("#rphlabel").css("color","black");
     console.log("valid: "+valid);
     //add phone number field
-                                                 
+
     if(!valid){
         $("#caMsg").html("error: enter all fields correctly, incorrect fields" +
                          " highlighted red");
@@ -103,10 +129,26 @@ function createAccount(form){
     }
     
     //hash the password
-    
-    //change to a synchronous $.ajax 
-    $.post("", {}, //fill out the JSON info
-           function(jsonData) {
+    var PHash = CryptoJS.MD5($("#registerPassword").val()).toString();
+    var rusrname = $("#registerUsername").val();
+    var rphone = $("#registerPhone").val();
+    var remail = $("#registerEmail").val();
+
+
+    //synchronous $.ajax for simpler async use $.post or $.get
+    $.ajax({
+          url: "http://boutinvm.eastus.cloudapp.azure.com/Distribute.svc/NewUser", 
+          type: "POST",
+          dataType: 'json',          
+          async: false,
+          contentType: "application/json",
+          data: {
+                   "UserName":rusrname,
+                   "PasswordHash":PHash,
+                   "PhoneNumber":rphone,
+                   "EmailAddress":remail
+          },
+          success: function(jsonData) {
              var rspData = $.parseJSON(jsonData);
              //$("#caMsg").html(rspData.MsgNo +" - "+ resData.msg);
              //login the new account
@@ -116,15 +158,18 @@ function createAccount(form){
              UserName = rspData.UserName;
              loggedIn = true;
              //create cookie
+             alert(rspData);
              createCookie(UserId, UserName);
+           },
+           error: function(xhr,ajaxOptions,thrownError){
+              alert("ERROR:" + xhr.responseText+" - "+thrownError);
            }
-           );
+           });
     return true;
     
 }
 
 //Destination specifies what page is being loaded
-//msgBool specifies whether or not to display a pop-up message
 //Function validates the existing session before users can access protected data
 //synchronous?
 function validateSession(dest){
@@ -146,7 +191,7 @@ function validateSession(dest){
               }
               )*/
    var pathArr = dest.split('/');
-   var page = pathArr[pathArr.length - 1];
+   var page = pathArr[pathArr.length - 1]; //this works to get the page name
    alert(page);
    var c = document.cookie;
    var decodedCookie = decodeURIComponent(document.cookie);
@@ -169,32 +214,39 @@ function validateSession(dest){
 //synchronous
 function login(form){
 
-   var Phash = //hash pass w/ MD5 hash
-   //synchronous login so the users can not change page while attempting to login
+   var lusr = $("#loginUsername").val();
+   var Phash = CryptoJS.MD5($("#loginPassword").val()).toString();
+   //synchronous login so the users can not change page while attempting to login   
+
    $.ajax({
-        url: "",
+        url: "http://boutinvm.eastus.cloudapp.azure.com/Distribute.svc/Authenticate",
         method: post,
         async: false,
+        contentType:"application/json",
+        dataType:'json',
         data: {
-           "UserName" : form.UserName.value,
+           "UserName" : lusr,
            "PasswordHash" : Phash
         },
         success: function(jsonData){
            //parse
            var rspData = $.parseJSON(jsonData);
            if(rspData.Status == 0){
+              //any other error handling
               alert("Username and Password do not match");
               return;
            } 
            UserId = rspData.UserId;
            UserName = rspData.UserName;
            loggedIn = true;
-           //change page
+           //create the cookie
+
+           //change page?
         },
         error: function(){
            alert("AJAX error");
         }
-     }
+     });
 }
 
 //Get rid of the token/cookie
@@ -208,10 +260,10 @@ function logout(){
 
 function createCookie(userId, userName){
    var d = new Date();
-   d.setTime(d.getTime() + (20*24*60*60*1000));
+   d.setTime(d.getTime() + (14*24*60*60*1000));
    var expires = "expires="+d.toUTCString();
    document.cookie = "id="+userId+";"+"usrName="+userName+
-                     ";"+expires+";path=/;";
+                     ";"+expires+";path=/";
 }
 
 
